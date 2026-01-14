c0ac75c1c9f614f229e8d8cbf141bc5795e1cbda
hyeonmin02 <hyenmin5@gmail.com>
2026-01-14 17:55:46 +0900

    add try catch

--- src/example/Calculator.java
+++ src/example/Calculator.java
@@ -1,18 +1,43 @@
 package example;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Hello,Calculator!");
         Scanner sc = new Scanner(System.in);
+        boolean run = true;
+        int a = 0;
+        int b = 0;
+        while (run) {
+            while (run) {
+                try {
+                    System.out.print("첫번째 숫자를 입력하시오: ");
+                    a = sc.nextInt();
+                    if (a < 0) {
+                        System.out.println("정수를 입력해주세요");
 
-        while (true) {
-            System.out.print("첫번째 숫자를 입력하시오: ");
-                int a = sc.nextInt();
+                        continue;
+                    }
+//                  break;
+                } catch (InputMismatchException e) {
+                    System.out.println("정수만 입력해주세요!");
+                    sc.nextLine();
+                }
 
-                System.out.print("두번째 숫자를 입력하시오: ");
-                int b = sc.nextInt();
-
+                while (run) {
+                    try {
+                        System.out.print("두번째 숫자를 입력하시오: ");
+                        b = sc.nextInt();
+                        if (b < 0) {
+                            System.out.print("정수를 입력해주세요");
+                            continue;
+                        }
+                        break;
+                    } catch (InputMismatchException e2) {
+                        System.out.print("정수를 입력해주세요");
+                        sc.nextLine();
+                    }
+                }
                 System.out.print("사칙연산 기호를 입력하시오: ");
                 char c = sc.next().charAt(0);
 
@@ -40,19 +65,26 @@ public class Calculator {
                         result = a / b;
                         break;
 
-                        default: //잘못된 입력값
+                    default: //잘못된 입력값
                         System.out.println("잘못된 입력입니다.");
                         continue;
                 }
                 System.out.println("결과: " + result);
 
-                System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
-                String cmd = sc.next();
-                if (cmd.equals("exit")) {
-                    System.out.println("계산기가 꺼집니다.");
-                    break;
+                while (run) {
+                    System.out.print("더 계산하시겠습니까? ( yes / exit ): ");
+                    String cmd = sc.next();
+
+                    if (cmd.equals("yes")) {
+                        break;
+                    } else if (cmd.equals("exit")) {
+                        System.out.println("계산기가 꺼집니다.");
+                        return;
+                    } else {
+                        System.out.println("오류: yes 또는 exit만 입력할 수 있습니다.");
+                    }
                 }
             }
         }
     }
-    // 양수 제외한 문자와 음수 입력시 돌려보내기 , exit 외에 다른 문자를 입력했을 시
+}
