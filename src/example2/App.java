3251925b575fc50723d09d9120ee52727a626c69
hyeonmin02 <hyenmin5@gmail.com>
2026-01-14 21:03:03 +0900

    계산기 동작 추가->결과값 삭제,계속,종료/final add

--- src/example2/App.java
+++ src/example2/App.java
@@ -19,17 +19,28 @@ public class App {
             char c = sc.next().charAt(0);
 
             try {
-                int result = cal.calculate(a,b,c); // 계산 + 저장 + 반환
+                int result = cal.calculate(a, b, c); // 계산 + 저장 + 반환
                 System.out.println("결과: " + result); // 계산 요청
                 System.out.println("누적 결과들: " + cal.getResults());
-            } catch (RuntimeException e) { //
+            } catch (DivideException e) { //
                 System.out.println(e.getMessage());
             }
-
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료): ");
-            if (sc.next().equalsIgnoreCase("exit"))
+            System.out.println("다음 동작 선택 → [yes:계속 / remove:첫 결과 삭제 / exit:종료] : ");
+            String cmd = sc.next();
+            if (cmd.equalsIgnoreCase("yes")) {
+                System.out.println("다시 계산을 계속합니다.");
+                continue;
+            }
+            if (cmd.equalsIgnoreCase("remove")) {
+                cal.removeResults();
+                System.out.println("현재 결과 목록: " + cal.getResults());
+                continue;
+            }
+            if (cmd.equalsIgnoreCase("exit")) {
+                System.out.println("계산기를 종료합니다.");
                 break;
+            }
+            System.out.println("잘못된 입력입니다. yes, remove, exit 중 하나를 입력해주세요");
         }
     }
 }
--- src/example2/Calculator.java
+++ src/example2/Calculator.java
@@ -3,14 +3,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    private final List<Integer> results = new ArrayList<>();
+    private final List<Integer> results = new ArrayList<>(); // 계산 결과를 자동으로 쌓아두는 상자 생성
+
+    public List<Integer> getResults() {
+        return results; // 결과 조회 버튼
+    }
+
+    public void removeResults() { // 먼저 저장된 결과 삭제 버튼
+        if (results.isEmpty()) {
+            System.out.println("삭제할 결과가 없습니다.");
+            return;
+        }
+        results.remove(0);
+        System.out.println("가장 먼저 저장된 결과를 삭제하였습니다.");
+    }
 
     public int calculate(int a, int b, char c) {
         int result;
 
         switch (c) {
             default: //잘못된 입력값
-                throw new DivideException("지원하지 않는 연산자입니다." + c);
+                throw new DivideException( c +"는 지원하지 않는 연산자입니다.");
 
             case '+': // 더하기
                 result = a + b;
@@ -36,8 +49,8 @@ public class Calculator {
         return result; // 결과 반환
     }
 
-    public List<Integer> getResults() {
-        return results;
+
     }
-} // s
+
+
 
